9e7225f31aaae14f38d6b5af0116aa0e38e94e3f
JoJo <jovannyclon@gmail.com>
2023-07-04 23:35:47 +0500

    Две задачи

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая:
+// 1. На вход принимает число
+// 2. Выдаёт его квдрат
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+//string str = Console.Readline()
+//string str = Console.ReadLine();
+Console.WriteLine("Ведите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
