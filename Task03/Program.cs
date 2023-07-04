21af14984263acab5d5ef1db30d3face7f026aee
JoJo <jovannyclon@gmail.com>
2023-07-05 00:05:48 +0500

    4

---
+++ Task03/Program.cs
@@ -0,0 +1,23 @@
+﻿// 7.Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю цифру
+// этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+{
+    number *= -1;
+}
+
+if (number >= 100 && number <= 999)
+{
+    int lastDigit = number % 10; // 456 % 10 -> 6
+    Console.Write($"Последняя цифра числа: {lastDigit}");
+
+}
+else
+{
+    Console.Write("Ошибка ввода");
+}
